7d37fe642e7296a6d17455b037a019d5ac2a1f03
Mrdestroyer <brian2k14hl@gmail.com>
2021-10-18 20:52:11 -0400

    Init commit

---
+++ Controllers/ContribuyenteController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RNC_API.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RNC_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContribuyenteController : ControllerBase
+    {
+        private companias_dbContext _context = new companias_dbContext();
+        // GET: api/<ContribuyenteController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Contribuyente>> Get()
+        {
+            return Ok("");
+        }
+
+        // GET api/<ContribuyenteController>/5
+        [HttpGet("{rnc}")]
+        public ActionResult<Contribuyente> Get(string rnc)
+        {
+            var contribuyente = _context.Contribuyentes
+                .Where(s => s.Rnc == rnc).FirstOrDefault<Contribuyente>();
+            
+            return Ok(contribuyente);
+        }
+
+        //// POST api/<ContribuyenteController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+            
+        //}
+
+        //// PUT api/<ContribuyenteController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+     
+        //}
+
+        //// DELETE api/<ContribuyenteController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Modelo/Contribuyente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace RNC_API.Modelo
+{
+    public partial class Contribuyente
+    {
+        public int Id { get; set; }
+        public string Rnc { get; set; }
+        public string RazonSocial { get; set; }
+        public string NombreComercial { get; set; }
+    }
+}
